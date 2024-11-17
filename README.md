7ea3f94d7c38e860d8b7787d07d06e7f590153ad
Akmal Fadhlurrahman <akmalf@users.noreply.github.com>
2024-11-17 17:20:45 +0700

    Write README.md

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # airport_ds
- Take-home project for Seinor Data Scientist role
+ Take-home project for Senior Data Scientist role 
+
+# Objectives and Key Results
+
+**Objective**: To design and build a scalable system for processing and analyzing real-time airport and flight data. The solution must efficiently handle large datasets from sources like ADSB and OAG, enabling effective analysis to support operational and strategic needs.
+
+**Key Results**:
+
+1. xxx
+2. xxx
+3. xxx
+
+
+# How to Replicate the Result
+
+The instruction below is for Windows-based installation. If you use other OS, your mileage may vary.
+
+## Install Apache Spark
+
+1. Install Java JDK 1.8 from the [following link](https://www.oracle.com/id/java/technologies/javase/javase8u211-later-archive-downloads.html). Ensure you know where the installation folder is located -- you will need it later.
+
+2. Download Apache Spark from [official website](https://spark.apache.org/downloads.html). Extract the `spark-3.x.y-bin-hadoop3.tgz` file to a folder of your choice. (for example, in `C:\spark\`)
+
+## Python installation
+1. Install Python 3.9 using your preferred package manager or download the installation file from https://www.python.org/downloads/.
+
+2. On a folder of your choice, create `airport_ds` virtual environment in Python 3.9 by running
+```
+python -m venv airport_ds
+```
+
+3. Activate the environment
+```
+.\airport_ds\Scripts\activate
+```
+
+4. Install all required packages using the available `requirements.txt`. This will install, among others, the `pyspark` package.
+```
+python -m ensurepip
+python -m pip install -r requirements.txt
+```
+
+## Running the project
+
+1. On the project folder, create `.env` file with the following contents:
+```
+SPARK_HOME=path\to\spark\spark-3.x.y-bin-hadoop3\
+JAVA_HOME=path\to\jre1.8.0_431\
+```
+
+2. Run the `project.py`
+```
+python project.py
+```
+3. The results will be stored in `.\results\`
+
--- project.py
+++ project.py
@@ -1,17 +1,10 @@
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode 
+from pyspark.sql.functions import col, explode
 
 # Define OS environments
 os.environ['SPARK_HOME'] = os.getenv('SPARK_HOME')
 os.environ['JAVA_HOME'] = os.getenv('JAVA_HOME')
-
-# Initialize SparkSession
-spark = SparkSession.builder.appName("AirportKPI").getOrCreate()
-
-# Read the ADSB dataset
-adsb_path = "./data/adsb.json"
-adsb_df = spark.read.option("multiline","true").json(adsb_path).show()
 
 def parse_adsb(spark, file_path):
     try:
@@ -21,9 +14,78 @@ def parse_adsb(spark, file_path):
     except Exception as e:
         print(f"Error parsing ADSB file: {e}")
         return None
-        
 
-# Read the OAG dataset
-oag_path = "./data/oag.json"
-oag_df = spark.read.option("multiline","true").json(oag_path)
-oag_df = oag_df.withcolumn("iata_aircraft", col("data.aircraftType")).show()
+def parse_oag(spark, file_path):
+    try:
+        df = spark.read.option("multiline","true").json(file_path)
+        df = df.select(explode(col("data")).alias("D"))
+        df = df.select(
+            col("D.carrier.iata").alias("iata_carrier_cd"),
+            col("D.carrier.icao").alias("icao_carrier_cd"),
+            col("D.serviceSuffix").alias("service_suffix"),
+            col("D.flightNumber").alias("flight_num"),
+            col("D.sequenceNumber").alias("seq_num"),
+            col("D.flightType").alias("flight_type"),
+            col("D.departure.airport.iata").alias("departure_port_cde"),
+            col("D.departure.terminal").alias("departure_terminal"),
+            col("D.departure.date.utc").alias("departure_day"),
+            col("D.departure.time.utc").alias("departure_time"),
+            col("D.arrival.airport.iata").alias("arrival_port_cde"),
+            col("D.arrival.terminal").alias("arrival_terminal"),
+            col("D.arrival.date.utc").alias("arrival_day"),
+            col("D.arrival.time.utc").alias("arrival_time"),
+            col("D.elapsedTime").alias("elapsed_time"),
+            col("D.aircraftType.iata").alias("iata_aircraft_typ"),
+            col("D.serviceType.iata").alias("iata_service_typ"),
+            col("D.segmentInfo.numberOfStops").alias("stops_cnt"),
+            col("D.segmentInfo.intermediateAirports.iata").alias("iata_int_port"),
+            col("D.scheduleInstanceKey").alias('schedule_inst_key'),
+            col("D.statusKey").alias('status_key'),
+            explode(col("D.statusDetails.state")).alias("stat_state"),
+            explode(col("D.statusDetails.updatedAt")).alias("stat_updated_at"),
+            explode(col("D.statusDetails.departure.estimatedTime.outGateTimeliness")).alias("departure_est_outgte_timely"),
+            explode(col("D.statusDetails.departure.estimatedTime.outGateVariation")).alias("departure_est_outgte_var"),
+            explode(col("D.statusDetails.departure.estimatedTime.outGate.utc")).alias("departure_est_outgte_time"),
+            explode(col("D.statusDetails.departure.actualTime.outGateTimeliness")).alias("departure_act_outgte_timely"),
+            explode(col("D.statusDetails.departure.actualTime.outGateVariation")).alias("departure_act_outgte_var"),
+            explode(col("D.statusDetails.departure.actualTime.outGate.utc")).alias("departure_act_outgte_time"),
+            explode(col("D.statusDetails.departure.actualTime.offGround.utc")).alias("departure_act_offgr_time"),
+            explode(col("D.statusDetails.departure.airport.iata")).alias("stat_departure_iata_cde"),
+            explode(col("D.statusDetails.departure.actualTerminal")).alias("stat_departure_act_terminal"),
+            explode(col("D.statusDetails.departure.gate")).alias("stat_departure_gte"),
+            explode(col("D.statusDetails.departure.checkInCounter")).alias("stat_departure_chckin_cntr"),
+            explode(col("D.statusDetails.arrival.estimatedTime.inGateTimeliness")).alias("arrival_est_ingte_timely"),
+            explode(col("D.statusDetails.arrival.estimatedTime.inGateVariation")).alias("arrival_est_ingte_var"),
+            explode(col("D.statusDetails.arrival.estimatedTime.inGate.utc")).alias("arrival_est_ingte_time"),
+            explode(col("D.statusDetails.arrival.actualTime.inGateTimeliness")).alias("arrival_act_ingte_timely"),
+            explode(col("D.statusDetails.arrival.actualTime.inGateVariation")).alias("arrival_act_ingte_var"),
+            explode(col("D.statusDetails.arrival.actualTime.inGate.utc")).alias("arrival_act_ingte_time"),
+            explode(col("D.statusDetails.arrival.actualTime.onGround.utc")).alias("arrival_act_ongr_time"),
+            explode(col("D.statusDetails.arrival.airport.iata")).alias("stat_arrival_iata_cde"),
+            explode(col("D.statusDetails.arrival.actualTerminal")).alias("stat_arrival_act_terminal"),
+            explode(col("D.statusDetails.arrival.baggage")).alias("stat_arrival_bgge")
+        )
+
+        return df
+    
+    except Exception as e:
+        print(f"Error parsing ADSB file: {e}")
+        return None
+
+if __name__ == "__main__":
+
+    # Initialize SparkSession
+    spark = SparkSession.builder.appName("AirportKPI").getOrCreate()
+
+    # Parse ADSB JSON into Spark DataFrame
+    adsb_filepath = '.\\data\\adsb.json'
+    adsb_df = parse_adsb(spark, adsb_filepath)
+
+    # Parse OAG JSON into Spark DataFrame
+    oag_filepath = '.\\data\\oag.json'
+    oag_df = parse_oag(spark, oag_filepath)
+
+    
+
+
+    
