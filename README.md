d8b5042d71870483642108efbba63807ca743278
Akmal Fadhlurrahman <akmalf@users.noreply.github.com>
2024-11-17 19:24:46 +0700

    Data visualization

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
 # airport_ds
- Take-home project for Senior Data Scientist role 
+ Take-home project for a Senior Data Scientist role 
 
 # Objectives and Key Results
 
 **Objective**: To design and build a scalable system for processing and analyzing real-time airport and flight data. The solution must efficiently handle large datasets from sources like ADSB and OAG, enabling effective analysis to support operational and strategic needs.
 
-**Key Results**:
+**Results**:
 
-1. xxx
-2. xxx
-3. xxx
+1. The average origin airport speed is
+
+|Origin Airport|avg(Speed)|
+| --- | ----------- |
+|   GUA|     170.0|
+|   DOH|     250.0|
+|   SGN|     234.2|
+|   IAD|     170.0|
+
+
+2. The average destination airport speed is
+
+|Destination Airport|avg(Speed)|
+| --- | ----------- |
+|        BED|     170.0|
+|        SYZ|     250.0|
+|        ICN|     234.2|
+|        MIA|     170.0|
+
+3. The total of departure and arrival data are:
+
+|sum(departure_delay)|sum(arrival_delay)|
+| --- | ----------- |
+|                   5|                 4|
+
+4. The most recent entry of each flights are:
+|FlightId|         LastUpdate|
+| --- | ----------- |
+|  AAL476|2023-10-03 23:36:00|
+|   BA484|2023-10-03 06:47:00|
+|  LXJ476|2023-10-03 23:25:20|
+|   QR476|2023-10-03 06:12:15|
+
+The data visualisation are available in `results` folder
 
 
 # How to Replicate the Result
@@ -18,7 +49,7 @@ The instruction below is for Windows-based installation. If you use other OS, yo
 
 ## Install Apache Spark
 
-1. Install Java JDK 1.8 from the [following link](https://www.oracle.com/id/java/technologies/javase/javase8u211-later-archive-downloads.html). Ensure you know where the installation folder is located -- you will need it later.
+1. Install Java JRE 1.8 (for example, you will install `jre1.8.0_abc`) from the [following link](https://www.oracle.com/id/java/technologies/javase/javase8u211-later-archive-downloads.html). Ensure you know where the installation folder is located -- you will need it later.
 
 2. Download Apache Spark from [official website](https://spark.apache.org/downloads.html). Extract the `spark-3.x.y-bin-hadoop3.tgz` file to a folder of your choice. (for example, in `C:\spark\`)
 
@@ -46,8 +77,14 @@ python -m pip install -r requirements.txt
 1. On the project folder, create `.env` file with the following contents:
 ```
 SPARK_HOME=path\to\spark\spark-3.x.y-bin-hadoop3\
-JAVA_HOME=path\to\jre1.8.0_431\
-```
+JAVA_HOME=path\to\jre1.8.0_abc\
+``` 
+If the program won't run, add the following variables to user's environment variables:
+
+| Variable | Value |
+| --- | ----------- |
+| SPARK_HOME | `path\to\spark\spark-3.x.y-bin-hadoop3\` |
+| JAVA_HOME | `path\to\jre1.8.0_abc\` |
 
 2. Run the `project.py`
 ```
--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 import os
+import matplotlib.pyplot as plt
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode
+from pyspark.sql.functions import col, explode, mean, row_number, desc, to_timestamp
+from pyspark.sql.window import Window
+from pyspark.sql.types import TimestampType
 
 # Define OS environments
 os.environ['SPARK_HOME'] = os.getenv('SPARK_HOME')
@@ -85,7 +88,110 @@ if __name__ == "__main__":
     oag_filepath = '.\\data\\oag.json'
     oag_df = parse_oag(spark, oag_filepath)
 
+    ##############################################################
+    # Calculate basic airport KPI
+    ##############################################################
+
+    # Airport Speed
+    avg_airport_speed = {
+        'by_origin' : adsb_df.groupBy("Origin").agg(mean("Speed")),
+        'by_destination' : adsb_df.groupBy("Destination").agg(mean("Speed"))
+    }
+
+    speed_origin = avg_airport_speed['by_origin']
+    speed_destination = avg_airport_speed['by_destination']
+
+    # Number of delayed flights by arrival delays and departure delays
+    delay_key = 'Delayed'
+    dep_arr_cols = {
+        'departure': 'departure_act_outgte_timely',
+        'arrival':  'arrival_act_ingte_timely'
+    }
+    delays = oag_df.select([(col(t2) == delay_key).cast("int").alias(f"{t1}_delay") 
+                   for (t1,t2) in dep_arr_cols.items()]).groupBy().sum()
+
+    print("The total non-delays are as follows:")
+    non_delays = oag_df.select([(col(t2) != delay_key).cast("int").alias(f"{t1}_non_delay") 
+                   for (t1,t2) in dep_arr_cols.items()]).groupBy().sum()
+
+
+    ##############################################################
+    # Filtering and transforming DataFrame
+    ##############################################################
+
+    # Filter the DataFrame to retain only the most recent entry 
+    # (the one with the largest LastUpdate) for each FlightId.
+    # Return a DataFrame containing only the FlightId and the 
+    # corresponding latest LastUpdate.
+    w_lastupdate = Window.partitionBy("Flight").orderBy(desc("LastUpdate"))
+    transforms = {
+        "row" : row_number().over(w_lastupdate),
+        "lastUpdate" : to_timestamp(adsb_df.LastUpdate.cast(dataType=TimestampType()))
+    }
+    entry = adsb_df.withColumns(transforms)\
+        .filter(col("row") == 1)\
+        .drop("row")\
+        .select(col('Flight').alias('FlightId'),col('LastUpdate'))
     
+    ##############################################################
+    # Plotting the result and save it in .\results\
+    ##############################################################
+
+    dest_folder = ".\\results\\"
+
+    # Airport speed (origin)
+    speed_origin_pd = speed_origin.toPandas()
+    plt.figure(figsize=(8,6))
+    plt.bar(speed_origin_pd["Origin"], speed_origin_pd["avg(Speed)"])
+    plt.title('Speed by Origin Airport')
+    plt.xlabel('Airport')
+    plt.ylabel('Average Speed')
+    # Save the plot as a JPG file
+    plt.savefig(f"{dest_folder}speed_origin.jpg", format='jpg', dpi=300)  # dpi=300 for high resolution
+
+    # Airport speed (destination)
+    speed_destination_pd = speed_destination.toPandas()
+    plt.figure(figsize=(8,6))
+    plt.bar(speed_destination_pd["Destination"], speed_origin_pd["avg(Speed)"])
+    plt.title('Speed by Destination Airport')
+    plt.xlabel('Airport')
+    plt.ylabel('Average Speed')
+    # Save the plot as a JPG file
+    plt.savefig(f"{dest_folder}speed_destination.jpg", format='jpg', dpi=300)  # dpi=300 for high resolution
+    
+    # Delays
+    delays_pd = delays.toPandas()
+    non_delays_pd = non_delays.toPandas()
+    fig, (ax1,ax2) = plt.subplots(2)
+    ax1.axis('tight')
+    ax1.axis('off')
+    ax2.axis('tight')
+    ax2.axis('off')
+    fig.suptitle("Delays and Non-delays")
+    ax1.table(cellText=delays_pd.values, colLabels=delays_pd.columns, loc='center', cellLoc='center')
+    ax2.table(cellText=non_delays_pd.values, colLabels=non_delays_pd.columns, loc='center', cellLoc='center')
+    # Save the plot as a JPG file
+    plt.savefig(f"{dest_folder}delays.jpg", format='jpg', dpi=300)  # dpi=300 for high resolution
+
+    # Last_entry
+    entry_pd = entry.toPandas()
+    fig, ax = plt.subplots(1)
+    ax.axis('tight')
+    ax.axis('off')
+    fig.suptitle("Most recent entry of each flights")
+    ax.table(cellText=entry_pd.values, colLabels=entry_pd.columns, loc='center', cellLoc='center')
+    # Save the plot as a JPG file
+    plt.savefig(f"{dest_folder}last_entry.jpg", format='jpg', dpi=300)  # dpi=300 for high resolution
+
+
+
+
+
+
+
+
+
+
 
 
     
